037b5c7073bd167bca0c33cb2482082cf99ec1a6
CihanSonmez <cihansonmez11@gmail.com>
2020-01-18 23:46:28 +0300

    Add files via upload

---
+++ Section5/LargestPrime.java
@@ -0,0 +1,43 @@
+/*
+Write a method named getLargestPrime with one parameter of type int named number. 
+
+If the number is negative or does not have any prime numbers, the method should return -1 to indicate an invalid value.
+
+
+The method should calculate the largest prime factor of a given number and return it.
+
+
+EXAMPLE INPUT/OUTPUT:
+
+* getLargestPrime (21); should return 7 since 7 is the largest prime (3 * 7 = 21)
+
+* getLargestPrime (217); should return 31 since 31 is the largest prime (7 * 31 = 217)
+
+* getLargestPrime (0); should return -1 since 0 does not have any prime numbers
+
+* getLargestPrime (45); should return 5 since 5 is the largest prime (3 * 3 * 5 = 45)
+
+* getLargestPrime (-1); should return -1 since the parameter is negative
+
+
+HINT: Since the numbers 0 and 1 are not considered prime numbers, they cannot contain prime numbers.
+
+NOTE: The method getLargestPrime should be defined as public static like we have been doing so far in the course.
+
+NOTE: Do not add a main method to the solution code.
+*/
+public class LargestPrime {
+    public static int getLargestPrime(int number){
+        
+        int[] prm = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997};
+        if(number<2) return -1;
+        else{
+            for(int i=prm.length-1;i>-1;i--){
+            
+               if(number%prm[i]==0) return prm[i];
+        
+            }
+            return -1;
+        }
+    }
+}
